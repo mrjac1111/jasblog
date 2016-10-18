9335fa74a98515e7a84408db46b2969ba3785e23
Jawad Ahmad <mrjac1111@gmail.com>
2016-10-17 23:29:26 -0700

    blog first page responsive (FINAL)

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                 for computer system manufacturers such as Apple Inc., Lenovo, HP and Dell. Intel also manufactures
                 motherboard chipsets, network interface controllers and integrated circuits, flash memory, graphics chips,
                 embedded processors and other devices related to communications and computing.</p>
-            <div class="read-more"><a href="readMore.php"><p> Read More</p></a></div>
+          <div class="read-more"><a href="readMore.php"><p> Read More</p></a></div>
         </div>
 
 
@@ -58,7 +58,36 @@
 
     </div>
     <!-- End of Post -->
+    <!-- End of Post -->
+    <div class="post">
+        <div class="post-left-content">
+            <div class="user-img">
+                <img src="img/Icon-user.png">
+            </div>
+            <div class="user-name">
+                <h3>User Name</h3>
+            </div>
+            <div class="rating">
+                <img src="img/rating.png"/>
+            </div>
+        </div>
+        <div class="post-right-content">
+            <h2>Post Title</h2><hr>
+            <p>Intel Corporation (also known as Intel and stylized as intel) is an American multinational
+                technology company headquartered in Santa Clara, California. It is one of the world's largest
+                and highest valued semiconductor chip makers based on revenue.,[3] and is the inventor of the x86
+                series of microprocessors, the processors found in most personal computers (PCs). Intel supplies processors
+                for computer system manufacturers such as Apple Inc., Lenovo, HP and Dell. Intel also manufactures
+                motherboard chipsets, network interface controllers and integrated circuits, flash memory, graphics chips,
+                embedded processors and other devices related to communications and computing.</p>
+                 <div class="read-more"><a href="readMore.php"><p> Read More</p></a></div>
+        </div>
 
+
+
+    </div>
+    <!-- End of Post -->
+</div>
 <div class="content-right">
 
     <div class="navigation">
